ce4af19a92ea1e10e790aa4398c7ffc776bbfdcc
BetoskeyLocal <gilberto_diaz_aguilar@hotmail.com>
2021-07-23 15:13:06 -0500

    correccion del branch

--- datos_estadisticos/datos_estadisticos.py
+++ datos_estadisticos/datos_estadisticos.py
@@ -8,7 +8,7 @@ from formulas_especiales import ceiling_to_a_number, floor_to_a_number
 class DatosEstadisticos:
 
     def __init__(self, datos, titulo, repr_xi, repr_fa, agrupados=False, columna_xi=0, columna_fa=0, xi_es_index=False, muestra=True):
-        
+        'me encuentro en branch creacion_tabla_estadistica'
 
         if type(datos) == list:
             self.datos = pandas.DataFrame(datos)
